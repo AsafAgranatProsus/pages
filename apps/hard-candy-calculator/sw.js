b53763a50ba30a45f2fa9bdbe3424109fdd5cd61
Nanobot <nanobot@asafagranat.com>
2026-08-27 18:33:47 +0000

    add hard-candy-calculator PWA

---
+++ apps/hard-candy-calculator/sw.js
@@ -0,0 +1,45 @@
+const CACHE_NAME = 'hard-candy-calc-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+self.addEventListener('install', function(event){
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache){
+      return cache.addAll(ASSETS);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', function(event){
+  event.waitUntil(
+    caches.keys().then(function(keys){
+      return Promise.all(
+        keys.filter(function(key){ return key !== CACHE_NAME; })
+            .map(function(key){ return caches.delete(key); })
+      );
+    })
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(cached){
+      if (cached) return cached;
+      return fetch(event.request).then(function(response){
+        if (response && response.status === 200 && event.request.method === 'GET'){
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then(function(cache){ cache.put(event.request, clone); });
+        }
+        return response;
+      }).catch(function(){
+        return caches.match('./index.html');
+      });
+    })
+  );
+});
